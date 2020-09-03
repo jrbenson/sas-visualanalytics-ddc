45546f56c6f49a81eaa17339fe1f82b34d3bfa59
Riley Benson <Riley.Benson@sas.com>
2020-09-03 14:26:05 -0400

    Simplified exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
-import {
+export { VAParameter, VAFormat, VAColumn, VAMessage } from './content'
+
+export {
   setupResizeListener,
   validateRoles,
   initializeSelections,
   convertDateColumns,
   getVAParameters,
 } from './content'
-export { setupResizeListener, validateRoles, initializeSelections, convertDateColumns, getVAParameters }
 
-import { VAParameter, VAFormat, VAColumn, VAMessage } from './content'
-export { VAParameter, VAFormat, VAColumn, VAMessage }
-
-import {
+export {
   setOnDataReceivedCallback,
   postSelectionMessage,
   postInstructionalMessage,
   postMessage,
   getUrlParams,
 } from './messaging'
-export { setOnDataReceivedCallback, postSelectionMessage, postInstructionalMessage, postMessage, getUrlParams }
-
-
