0cb0b6997d23db1d6abad5307fe7cc37983cdd59
Riley Benson <Riley.Benson@sas.com>
2021-01-06 13:08:24 -0500

    Added parameter test.

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,24 @@
-import * as va from '../lib/index'
+import * as va from '../src/index'
 
-test('basic', () => {
-    expect(20).toBe(20)
+test('URL Parameter Parsing', () => {
+
+  global.window = Object.create(window)
+  Object.defineProperty(window, 'location', {
+    value: {
+      search: 'https://www.web.com/page?param1=value1&param2&param3=value3',
+    },
+    writable: true,
+  })
+
+  const params = va.getUrlParams()
+
+  expect(params['param1']).toBeDefined()
+  expect(params['param1']).toBe('value1')
+  expect(params['param2']).toBeDefined()
+  expect(params['param3']).toBeDefined()
+  expect(params['param3']).toBe('value3')
+
+  expect(va.getUrlParam('param1')).toBe('value1')
+  expect(va.getUrlParam('param2')).toBe('')
+  expect(va.getUrlParam('param3')).toBe('value3')
 })
