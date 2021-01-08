1458c4549d128c676167ef507b381eeb14752d69
Riley Benson <Riley.Benson@sas.com>
2021-01-07 23:07:32 -0500

    Simplified build to use only rolllup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,25 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { terser } from "rollup-plugin-terser"
+import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
 
-export default [ {
-    input: "./lib/index.js",
-    output: {
-        file: "dist/vaddc.js",
-        name: "ddc",
-        format: "iife"
-    },
-    plugins: [nodeResolve()]
-},
-{
-    input: "./lib/index.js",
-    output: {
-        file: "dist/vaddc.min.js",
-        name: "ddc",
-        format: "iife"
-    },
-    plugins: [nodeResolve(), terser()]
-}
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/vaddc.min.js',
+        name: 'ddc',
+        format: 'iife',
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+      },
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+      },
+    ],
+    plugins: [typescript(), nodeResolve(), terser()],
+  },
 ]
